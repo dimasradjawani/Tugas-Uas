674b46e9e35bf0c11e5045f46be15e41cef5b6a4
dimas arwindo radjawani <37289791+dimasradjawani@users.noreply.github.com>
2018-04-17 18:15:02 +0700

    Add files via upload

---
+++ UAS.py
@@ -0,0 +1,17 @@
+import dim.satu
+import dim.dua
+import dim.tiga
+import dim.empat
+import dim.enam
+
+print (dim.satu.fa())
+
+print (dim.dua.fb())
+
+print (dim.tiga.fc())
+
+print (dim.empat.fd())
+
+print (dim.lima.fe())
+
+print (dim.enam.ff())
